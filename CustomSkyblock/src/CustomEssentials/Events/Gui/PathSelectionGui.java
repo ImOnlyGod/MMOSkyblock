824504e0675878cfeb4fb2ddd753c3bb6eee3049
Abhiram Aroop <aaro0006@student.monash.edu>
2022-10-27 11:45:10 +1100

    Fixed rounding errors

--- CustomSkyblock/src/CustomEssentials/Events/Gui/PathSelectionGui.java
+++ CustomSkyblock/src/CustomEssentials/Events/Gui/PathSelectionGui.java
@@ -71,7 +71,7 @@ public class PathSelectionGui implements TabExecutor{
 		
 		//CHANGE TO ONLY BOW DMG -> ADD BOW DMG
 		ItemStack archer = new ItemStack(Material.BOW);
-		ItemMeta archerMeta = tank.getItemMeta();
+		ItemMeta archerMeta = archer.getItemMeta();
 		tankMeta.setDisplayName(Utils.chat("&6&l        Archer &7Path"));
 		tankMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 		tankMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
@@ -96,7 +96,7 @@ public class PathSelectionGui implements TabExecutor{
 		
 		
 		ItemStack assassin = new ItemStack(Material.REDSTONE);
-		ItemMeta assassinMeta = tank.getItemMeta();
+		ItemMeta assassinMeta = assassin.getItemMeta();
 		tankMeta.setDisplayName(Utils.chat("&4&l        Assassin &7Path"));
 		tankMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 		tankMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
