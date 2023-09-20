dcc38ab08645a2064720df538f13db4889d6cf3c
ImOnlyGod <abhiram.aroop@gmail.com>
2023-09-20 13:57:34 +1000

    Added Hunger back and golden carrot potion effects

--- CustomSkyblock/src/CustomEssentials/Events/EventTasks/FoodSaturation.java
+++ CustomSkyblock/src/CustomEssentials/Events/EventTasks/FoodSaturation.java
@@ -1,8 +1,15 @@
 package CustomEssentials.Events.EventTasks;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import CustomEssentials.Main;
 
 
@@ -16,8 +23,18 @@ public class FoodSaturation implements Listener{
 	
 	@EventHandler
 	public void HungerEvent(FoodLevelChangeEvent e) {
-		e.setCancelled(true);
+		e.setCancelled(false);
 		
+	}
+	
+	@EventHandler
+	public void eatFoodEvent(PlayerItemConsumeEvent e) {
+		ItemStack item = e.getItem();
+		Player p = e.getPlayer();
+		if (item.getType() == Material.GOLDEN_CARROT) {
+			p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,60,2));
+			p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP,60,1));
+		}
 	}
 		
 }
